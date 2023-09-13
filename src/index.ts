4f246afdf7215c7e2c555c307a51e40cfacf869a
Khazhi <khazhi404@gmail.com>
2023-09-13 23:30:01 +0300

    add versel.json

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,38 @@ import express, {Request, Response} from 'express'
 const app = express()
 const port = 3000
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
+
+const db = {
+    videos: [{
+        id: 1,
+        title: 'title',
+        author: 'author',
+        canBeDownloaded: false,
+        minAgeRestriction: 1,
+        nullable: true,
+        createdAt:	new Date(),
+        publicationDate: new Date(),//+1
+        availableResolutions: 'P2160'
+    },
+        {
+            id: 2,
+            title: 'title',
+            author: 'author',
+            canBeDownloaded: false,
+            minAgeRestriction: 1,
+            nullable: true,
+            createdAt:	new Date(),
+            publicationDate: new Date(),//+1
+            availableResolutions: 'P2160'
+        },
+
+    ]}
+
+
+app.get('/videos', (req: Request, res: Response) => {
+    res.send(db.videos)
 })
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
