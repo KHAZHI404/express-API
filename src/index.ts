b6bf2717c6d5d01f9cfe79ef699a0d2ca97da274
Khazhi <khazhi404@gmail.com>
2023-09-17 22:14:38 +0300

    add post and put endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,66 @@
 import express, {Request, Response} from 'express'
+import bodyParser from 'body-parser'
+// import cors from 'CORS'
+
 const app = express()
-const port = 3000
+
+// const corsMiddleware = cors();
+// app.use(corsMiddleware)
+const jsonBodyMiddleware = bodyParser.json()
+app.use(jsonBodyMiddleware)
+const port = process.env.PORT || 3000
 
 
-const db = {
-    videos: [{
-        id: 1,
-        title: 'title',
-        author: 'author',
-        canBeDownloaded: false,
-        minAgeRestriction: 1,
-        nullable: true,
-        createdAt:	new Date(),
-        publicationDate: new Date(),//+1
-        availableResolutions: 'P2160'
-    },
-        {
-            id: 2,
-            title: 'title',
-            author: 'author',
-            canBeDownloaded: false,
-            minAgeRestriction: 1,
-            nullable: true,
-            createdAt:	new Date(),
-            publicationDate: new Date(),//+1
-            availableResolutions: 'P2160'
-        },
-
-    ]}
+let videos = [{id: '1', title: 'Name 1', author: 'Author 1'}, {id: '2', title: 'Name 2', author: 'Author 2'}]
 
 
 app.get('/videos', (req: Request, res: Response) => {
-    res.send(db.videos)
+    res.send(videos)
+})
+app.post('/videos', (req: Request, res: Response) => {
+    let title = req.body.title
+    if (!title || !title.trim() || title.length > 40 || typeof title !== "string") {
+        res.status(400).send({
+            errorsMessages: [{
+                "message": "title",
+                "field": "errors in title"
+            }]
+        })
+        return
+    }
+
+    const newVideo = {
+        id: new Date().toISOString(),
+        title: title,
+        author: 'Leonardo'
+    }
+
+    videos.push(newVideo)
+
+    res.status(201).send(newVideo)
+})
+app.put('/videos:videoId', (req: Request, res: Response) => {
+    let title = req.body.title
+    if (!title || !title.trim() || title.length > 40 || typeof title !== "string") {
+        res.status(400).send({
+            errorsMessages: [{
+                "message": "title",
+                "field": "errors in title"
+            }]
+        })
+        return
+    }
+
+    const id = req.params.videoId;
+    const video= videos.find(v => v.id === id)
+    if (video) {
+        video.title = title;
+        res.status(204).send(video)
+    } else {
+        res.send(404)
+    }
+
+
 })
 
 
