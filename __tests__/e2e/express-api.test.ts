55da7dd050503221c908dba8424d4f1509ddcaba
Khazhi <khazhi404@gmail.com>
2023-09-22 20:21:33 +0300

    add HTTPS STATUSES and fix tests

--- __tests__/e2e/express-api.test.ts
+++ __tests__/e2e/express-api.test.ts
@@ -3,12 +3,27 @@ import {app} from "../../src";
 
 describe('/videos', () => {
 
+    beforeAll(async () => {
+        await request(app)
+            .delete('/testing/all-data')
+    })
+
     it('should return 200 and array', async () => {
         await request(app)
             .get('/videos')
-            .expect(200, [ { id: '1', title: 'Name 1', author: 'Author 1' },
-                { id: '2', title: 'Name 2', author: 'Author 2' }])
+            .expect(200, [])
     });
 
+    it('should return 404 for not existing course', async () => {
+        await request(app)
+            .get('/videos/123123241233')
+            .expect(404)
+    });
 
+    it('shouldnt return 404 for not existing course', async () => {
+        await request(app)
+            .post('/videos/123123241233')
+            .send({ title: 'something', author: '' })
+            .expect(404)
+    });
 })
--- src/index.ts
+++ src/index.ts
@@ -6,24 +6,26 @@ export const app = express()
 const jsonBodyMiddleware = bodyParser.json()
 app.use(jsonBodyMiddleware)
 const port = process.env.PORT || 5000
+export const HTTP_STATUSES = {
+    OK_200: 200,
+    CREATED_201: 201,
+    NO_CONTENT_204: 204,
 
+    BAD_REQUEST_400: 400,
+    NOT_FOUND_404: 404,
+}
 
 let videos = [{id: '1', title: 'Name 1', author: 'Author 1'}, {id: '2', title: 'Name 2', author: 'Author 2'}]
 
-
-app.delete('/', (req: Request, res: Response) => {
-    videos = []
-    res.send(204)
-})
 app.get('/videos', (req: Request, res: Response) => {
-    res.status(200).send(videos)
+    res.status(HTTP_STATUSES.OK_200).send(videos)
 })
 app.post('/videos', (req: Request, res: Response) => {
     let title = req.body.title
     let author = req.body.author
 
     if (!title || !title.trim() || title.length > 40 || typeof title !== "string") {
-        res.status(400).send({
+        res.status(HTTP_STATUSES.BAD_REQUEST_400).send({
             errorsMessages: [{
                 "message": "errors in title",
                 "field": "title"
@@ -33,7 +35,7 @@ app.post('/videos', (req: Request, res: Response) => {
         return
     }
     if (!author || !author.trim() || author.length > 40 || typeof author !== "string") {
-        res.status(400).send({
+        res.status(HTTP_STATUSES.BAD_REQUEST_400).send({
             errorsMessages: [{
                 "message": "errors in author",
                 "field": "author"
@@ -51,22 +53,22 @@ app.post('/videos', (req: Request, res: Response) => {
 
     videos.push(newVideo)
 
-    res.status(201).send(newVideo)
+    res.status(HTTP_STATUSES.CREATED_201).send(newVideo)
 })
 app.get('/videos/:videoId', (req: Request, res: Response) => {
     const id = req.params.videoId
     const foundVideo = videos.find(v => v.id === id)
     if (foundVideo) {
-        res.status(200).send(foundVideo)
+        res.status(HTTP_STATUSES.OK_200).send(foundVideo)
     } else {
-        res.send(404)
+        res.send(HTTP_STATUSES.NOT_FOUND_404)
     }
 })
 app.put('/videos/:videoId', (req: Request, res: Response) => {
     let title = req.body.title
     let author = req.body.author
     if (!title || !title.trim() || title.length > 40 || typeof title !== "string") {
-        res.status(400).send({
+        res.status(HTTP_STATUSES.BAD_REQUEST_400).send({
             errorsMessages: [{
                 "message": "errors in title",
                 "field": "title"
@@ -77,7 +79,7 @@ app.put('/videos/:videoId', (req: Request, res: Response) => {
     }
 
     if (!author || !author.trim() || author.length > 20 || typeof author !== "string") {
-        res.status(400).send({
+        res.status(HTTP_STATUSES.BAD_REQUEST_400).send({
             errorsMessages: [{
                 "message": "errors in author",
                 "field": "author"
@@ -92,9 +94,9 @@ app.put('/videos/:videoId', (req: Request, res: Response) => {
     if (foundVideo) {
         foundVideo.title = title;
         foundVideo.author = author
-        res.status(204).send(foundVideo)
+        res.status(HTTP_STATUSES.CREATED_201).send(foundVideo)
     } else {
-        res.send(404)
+        res.send(HTTP_STATUSES.NOT_FOUND_404)
     }
 
 })
@@ -103,12 +105,16 @@ app.delete('/videos/:videoId', (req:Request, res:Response) => {
     const newVideos = videos.filter(v => v.id !== id)
     if (newVideos.length < videos.length) {
         videos = newVideos
-        res.send(204)
+        res.send(HTTP_STATUSES.NO_CONTENT_204)
     } else {
-        res.send(404)
+        res.send(HTTP_STATUSES.NOT_FOUND_404)
     }
 })
 
+app.delete('/testing/all-data', (req: Request, res: Response) => {
+    videos = []
+    res.send(HTTP_STATUSES.NO_CONTENT_204)
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
